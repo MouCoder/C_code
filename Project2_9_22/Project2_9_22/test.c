ec75092c86828b5f00058544f6c1c4a4d4275125
MouCoder <2241059521@qq.com>
2020-09-22 22:12:57 +0800

    数组出界（死循环）

---
+++ Project2_9_22/Project2_9_22/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 0 };
+	for (i = 0; i < 12; i++)
+	{
+		arr[i] = 1;
+		printf("hhh");
+	}
+	return 0;
+}
