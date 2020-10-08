0b70e3052baad39f93e2a33cf8b87dec4255292b
MouCoder <2241059521@qq.com>
2020-10-08 21:16:09 +0800

    指针

---
+++ Project2_10_8/Project2_10_8/TEST.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//
+//	char* p1 = "abcdef";
+//	char* p2 = "abcdef";
+//
+//	if (arr1 == arr2)
+//	{
+//		printf("arr1 == arr2");
+//	}
+//	else
+//	{
+//		printf("arr1 != arr2");
+//	}
+//
+//	if (p1 == p2)
+//	{
+//		printf("p1 == p2");
+//	}
+//	else
+//	{
+//		printf("p1 != p2");
+//	}
+//	return 0;
+//}
+
+//数组指针
+
+//int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int(*p)[10] = arr;
+//
+////指针数组
+//
+//int* arr[10];
+
+
