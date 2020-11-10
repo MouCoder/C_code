6af63a8f1be24a98fa6b3083500ba1ca73ed79fc
MouCoder <2241059521@qq.com>
2020-11-10 15:44:44 +0800

    leetcode---判断大小写（判断单词书写格式）

---
+++ Project2-11-10（1）/Project2-11-10（1）/test.c
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+bool detectCapitalUse(char * word)
+{
+	if (strlen(word) == 1)
+		return true;
+	//如果单词的第一个字母是小写字母，则后边所有字母都必须是小写
+	if (*word >= 'a' && *word <= 'z')
+	{
+		while (*word)
+		{
+			if (*word >= 'a' && *word <= 'z')
+			{
+				word++;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	//单词的第一个单词是大写，要么后边所有字母都是小写要么后边所有字母都是大写，才可以
+	else
+	{
+		word++;
+		if (*word >= 'A' && *word <= 'Z')
+		{
+			while (*word)
+			{
+				if (*word >= 'A' && *word <= 'Z')
+				{
+					word++;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			while (*word)
+			{
+				if (*word >= 'a' && *word <= 'z')
+				{
+					word++;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+int main()
+{
+	printf("%d\n",detectCapitalUse("a"));
+	return 0;
+}
