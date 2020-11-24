51b01e46298410c3ea3f682c6d1de5dcd3ea3f68
MouCoder <2241059521@qq.com>
2020-11-24 21:34:06 +0800

    关于int=int+int问题的测试

---
+++ Project2-11-24（1）/Project2-11-24（1）/test.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 2122222222;
+//	int b = 2122222222;
+//	int c = (a + b) / 2;//Ô¤ÆÚ½á¹û£º2122222222
+//	printf("%d\n",c);
+//	return 0;
+//}
+
+//void test1()
+//{
+//	long long a = 2122222222;
+//	long long b = 2122222222;
+//	long long c;
+//	c = (a+b)/2;
+//}
+//
+//void test2()
+//{
+//	unsigned int a = 2122222222;
+//	unsigned int b = 2122222222;
+//	unsigned int c;
+//	c = (a + b) / 2;
+//}
+
+int main()
+{
+	int a = 2122222222;
+	float b = (float)a;
+
+	printf("%f\n",b);
+	return 0;
+}
