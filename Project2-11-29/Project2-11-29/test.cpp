9818b4fdefc123d6a61764409726141afd09c541
MouCoder <2241059521@qq.com>
2020-11-29 15:41:38 +0800

    字符串排序

---
+++ Project2-11-29/Project2-11-29/test.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+//char* bubbleSort(char* s,int length)
+//{
+//	char* tmp = (char*)malloc(sizeof(char)*(length+11));
+//	for (int i = 0; i < length - 2;i++)
+//	for (int j = i + 1; j < length - 1; j++)
+//	{
+//		if (s[i] > s[j])
+//		{
+//			tmp[i] = s[i];
+//		}
+//		else
+//		{
+//			tmp[i] = s[j];
+//		}
+//	}
+//	tmp[length - 1] = s[length - 1];
+//	tmp[length] = '\0';
+//	return tmp;
+//}
+//bool isAnagram(char * s, char * t)
+//{
+//	//问题：有效的字母异位词
+//	/*1.冒泡排序，先对两个字符串按照ASCII排序，然后在调用库函数比较两个字符串是否相等*/
+//	char* p1 = bubbleSort(s,strlen(s));
+//	char* p2 = bubbleSort(t,strlen(t));
+//	if (strlen(p1) == strlen(p2))
+//	if (strcmp(p1,p2))
+//		return true;
+//	return false;
+//}
+int main()
+{
+	/*char *s = "rat", *t = "car";
+	int ret = isAnagram(s,t);
+	printf("%d",ret);*/
+	char* a = "";
+
+	return 0;
+}
